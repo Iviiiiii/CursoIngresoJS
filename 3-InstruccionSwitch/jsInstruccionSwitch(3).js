84fd04104556eb5bd1df9de453038aa6926d2b84
alumno <alumno>
2018-02-16 10:32:03 -0300

    Clase Switch/Default

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,8 +3,16 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+    switch(mesDelAño)
+{
+        default:
+            alert("Este mes tiene 30 o más días");
+            break;
+    
+        case "Febrero":
+            alert("Este mes no tiene más de 29 días.");
+            break;
+}
 	
 
 
