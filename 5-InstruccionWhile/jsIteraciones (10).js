57fbd0cedb4aa2fcd63980d9152167629e821fb1
Iviiiiii <ivianbrujo@gmail.com>
2018-02-23 12:02:15 -0300

    Clase While

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -5,11 +5,13 @@ function Mostrar()
 	var contadorNegativos=0;
 	var contadorCeros=0;
 	var respuesta="si";
-	var numeroNegativo;
-	var numeroPositivo;
+	var numeroNegativo=0;
+	var numeroPositivo=0;
 	var promedioPositivos;
 	var promedioNegativos;
 	var diferencia;
+	var contadorPares=0;
+
 	while(respuesta!="no")
 	{
 		numero=prompt("diga numero");
@@ -19,36 +21,42 @@ function Mostrar()
 			numero=prompt("diga numero");
 			numero=parseInt(numero);
 		}
-	if(numero<0)
-	{
-		numeroNegativo=numero+numero;
-		contadorNegativos++;
+		if(numero<0)
+		{
+			numeroNegativo=numeroNegativo+numero;
+			contadorNegativos++;
+		}
+		else
+		{
+			if(numero>0)
+			{
+				numeroPositivo=numeroPositivo+numero;
+				contadorPositivos++;
+			}
+			else
+			{
+				contadorCeros++;
+			}
+		}
+		if(numero%2==0&&numero!=0)
+		{
+			contadorPares++;
+		}
+		respuesta=prompt("desea continuar?");
 	}
-	if(numero>0)
-	{
-		numeroPositivo=numero+numero;
-		contadorPositivos++;
-
-	}
-	if(numero==0)
-	{
-		contadorCeros++;
-	}
-	respuesta=prompt("desea continuar?");
-	}
-
+	
 promedioPositivos=numeroPositivo/contadorPositivos;
 promedioNegativos=numeroNegativo/contadorNegativos;
-diferencia=numeroPositivo-numeroNegativo;
+diferencia=numeroPositivo+numeroNegativo;
 
-document.write("1-"+numeroNegativo).value;
-document.write("2-"+numeroPositivo).value;
-document.write("3-"+contadorPositivos).value;
-document.write("4-"+contadorNegativos).value;
-document.write("5-"+contadorCeros).value;
-document.write("6"+).value;
-document.write("7-"+promedioPositivos).value;
-document.write("8-"+promedioNegativos).value;
-document.write("9-"+diferencia).value;
+document.write("1-"+numeroNegativo);
+document.write("2-"+numeroPositivo);
+document.write("3-"+contadorPositivos);
+document.write("4-"+contadorNegativos);
+document.write("5-"+contadorCeros);
+document.write("6-"+contadorPares);
+document.write("7-"+promedioPositivos);
+document.write("8-"+promedioNegativos);
+document.write("9-"+diferencia);
 
 }//FIN DE LA FUNCIÓN
