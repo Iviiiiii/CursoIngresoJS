4dcb95d6eca87b42d6ba568c20ab2bc30ff23444
Iviiiiii <36264967+Iviiiiii@users.noreply.github.com>
2018-03-21 12:29:49 -0300

    Ejercicios primeras clases de la cursada

---
+++ Lab Computacion Clase 1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nombre[41];
+    char localidad[41];
+    printf("Nombre:");
+    fflush(stdin);
+    scanf("%s",nombre);
+    printf("Localidad:");
+    fflush(stdin);
+    scanf("%s",localidad);
+    printf("Usted se llama %s y vive en %s",nombre,localidad);
+    return 0;
+}
