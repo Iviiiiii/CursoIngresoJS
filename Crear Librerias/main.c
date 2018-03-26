17cb078a99fe14498481f69d1bfb2ca44d97db4a
Iviiiiii <36264967+Iviiiiii@users.noreply.github.com>
2018-03-26 12:31:38 -0300

    Crear librerias

---
+++ Crear Librerias/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "reng.h"
+int main()
+{
+    int respuesta;
+    int c=6;
+    int d=8;
+    respuesta=sumar(c,d);
+    printf("La suma es de %d",respuesta);
+    return 0;
+}
